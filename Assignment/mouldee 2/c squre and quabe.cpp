644b4bc486b5af6d63902509f2fe6d40a453d019
Monilrabadiya <111180219+Monilrabadiya@users.noreply.github.com>
2022-09-07 15:14:05 +0530

    Add files via upload

---
+++ Assignment/mouldee 2/c squre and quabe.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+ int n,s,q;
+ printf("enter any num");
+ scanf("%d",&n);
+ s=n*n;
+ q=n*n*n;
+ printf("s=%d\nq=%d",s,q);
+    return 0;
+}
