4c96bbe9918c2d9f471c6f3faf134916815c2247
Monilrabadiya <111180219+Monilrabadiya@users.noreply.github.com>
2022-09-23 16:15:15 +0530

    Add files via upload

---
+++ Task/structur.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+struct 
+{
+    int no;
+    char name[50];
+}to;
+
+int main()
+{
+    to.no;
+    to.name;
+        printf("enter the number of person  =  ");
+        scanf("%d",&to.no);
+        printf("enter the name  =  ");
+        scanf("%s",&to.name);
+        printf("no of person =  %d\n",to.no);
+        printf("person name =  %s",to.name);
+    
+}
+
