2ca81c120f75d8f3cc769225fca43a2f60f814dc
Monilrabadiya <111180219+Monilrabadiya@users.noreply.github.com>
2022-11-16 12:49:42 +0530

    Add files via upload

---
+++ Assignment/modul 3/prime or not prime no.c
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+
+    int i,n,f=0;
+    printf("enter the no=");
+    scanf("%d",&n);
+    for
+    {
+        if(n%i==0)(i=2;i<n;i++)
+        {
+            f=1;
+            break;
+        }
+    }
+    if(f==0)
+    {
+        printf("prime no");
+    }
+    else
+    {
+        printf("not prime no");
+    }
+    
+    return 0;
+}
